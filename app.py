5d8f8c7d9c712529ca7892d5e815d76c5724a171
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2024-01-31 15:31:51 -0500

    Fix

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello_world():  # put application's code here
     return flask.send_file("Bobby_Dhanoolal_Resume.pdf")
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
