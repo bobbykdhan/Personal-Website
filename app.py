58a0ab3315ae2717c35160f46ea2a8dc37ad4670
Bobby Dhanoolal <bobbykdhan@users.noreply.github.com>
2024-07-27 17:51:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import uvicorn
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def present_resume():  # put application's code here
     return flask.send_file("Bobby_Dhanoolal_Resume.pdf")
 
 if __name__ == '__main__':
