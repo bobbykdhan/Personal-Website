00ff8c8a7b885d7ab1900632cb7d03b28ff55584
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2025-04-08 15:48:53 -0400

    download button

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -43,6 +43,16 @@ export default function Resume() {
               >
                 ATS Version
               </button>
+              <a
+                href={viewMode === 'fancy' ? '/bobby_dhanoolal_resume_fancy.pdf' : '/bobby_dhanoolal_resume.pdf'}
+                download
+                className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700 transition-colors flex items-center gap-2"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg>
+                Download
+              </a>
             </div>
           </div>
         </motion.div>
@@ -55,7 +65,7 @@ export default function Resume() {
         >
           {isClient ? (
             <iframe
-              src={viewMode === 'fancy' ? '/resume-fancy.pdf' : '/resume.pdf'}
+              src={viewMode === 'fancy' ? '/bobby_dhanoolal_resume_fancy.pdf' : '/bobby_dhanoolal_resume.pdf'}
               className="w-full h-[calc(100vh-12rem)]"
               title="Resume PDF Viewer"
             />
